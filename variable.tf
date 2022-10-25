31ba78ceb785bd5bdbe7ac7ac03cdb53b0ca6baf
Kwasi <rasbarbies@yahoo.com>
2022-10-25 03:18:33 +0100

    updated variable and data filess

--- variable.tf
+++ variable.tf
@@ -130,3 +130,22 @@ variable "secret_key" {
 
   type = string
 }
+
+
+#please delete the below script on your necxt edit 
+
+variable "username"{
+  description = "db login username"
+  default     = "admin"
+
+  type        = string
+}
+
+
+variable "password" {
+  description  = "db password"
+  default      = "lolofuentes"
+  type         = string
+}
+
+ 
